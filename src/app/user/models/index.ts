f7ee730b53aca6f4a611a55168ee02e5efaff3e6
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-01-10 17:45:23 +0200

    add models - AuthValidationMessages - resetPasswordValidationMessages - resetPasswordDto

--- src/app/user/models/index.ts
+++ src/app/user/models/index.ts
@@ -1,15 +1,42 @@
 export interface TokenDto {
-  AccessToken: string;
-  RefreshToken: string;
+  accessToken: string;
+  refreshToken: string;
 }
 
-export interface UserForAuthDto{
-  Email: string;
-  Password: string;
+export interface UserForAuthDto {
+  email: string;
+  password: string;
+}
+
+export interface ForgotPasswordDto {
+  email: string;
+  resetPasswordClientURI: string;
+}
+
+export interface ResetPasswordDto {
+  password: string;
+  confirm: string;
+  email: string;
+  token: string;
 }
 
 
-export interface ForgotPasswordDto{
-  Email: string;
-  ResetPasswordClientURI: string;
+export const AuthValidationMessages = {
+  email: {
+    required: 'Email is required',
+  },
+
+  password: {
+    required: 'Password is required',
+  },
+};
+
+
+export const resetPasswordValidationMessages:{[key:string]: {[key:string]: string}} = {
+  "password":{
+    required:"Password is required"
+  },
+  "confirm":{
+    match: "Password and confirmation does not match"
+  }
 }
