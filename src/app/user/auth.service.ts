50298ca94d77a2b11a2a785707a30e8ff639fb5d
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-01-11 15:52:32 +0200

    add reset Password request handler

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,27 +1,42 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ForgotPasswordDto, TokenDto, UserForAuthDto } from './models';
+import {
+  ForgotPasswordDto,
+  ResetPasswordDto,
+  TokenDto,
+  UserForAuthDto,
+} from './models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private loginPath = 'https://localhost:5001/api/authentication/login';
-  private forgotPasswordPath =
-    'https://localhost:5001/api/accounts/forgotPassword';
+  private forgotPasswordPath ='https://localhost:5001/api/accounts/forgotPassword';
+  private resetPasswordPath = 'https://localhost:5001/api/accounts/resetPassword';
+
   constructor(private http: HttpClient) {}
 
   login = (userForAuth: UserForAuthDto) => {
     return this.http.post<TokenDto>(this.loginPath, userForAuth, {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+        'content-type': 'application/json',
+        'accept': 'application/json',
       }),
     });
   };
 
   forgotPasswordRequest = (forgotPasswordDto: ForgotPasswordDto) => {
-    return this.http.post<any>(this.forgotPasswordPath, forgotPasswordDto);
+    return this.http.post<void>(this.forgotPasswordPath, forgotPasswordDto);
+  };
+
+  resetPassword = (resetPassword: ResetPasswordDto) => {
+    return this.http.post<void>(this.resetPasswordPath, resetPassword, {
+      headers:new HttpHeaders({
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      })
+    });
   };
 }
