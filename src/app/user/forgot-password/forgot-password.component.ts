fe165467f53ea877121f2da423cd35cbde3e1329
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-01-11 15:47:52 +0200

    handle http bad request error

--- src/app/user/forgot-password/forgot-password.component.ts
+++ src/app/user/forgot-password/forgot-password.component.ts
@@ -1,11 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
+import { GenericValidator } from 'src/app/shared/generic-validators';
 import { AuthService } from '../auth.service';
 import { ForgotPasswordDto } from '../models';
 
@@ -18,26 +15,34 @@ import { ForgotPasswordDto } from '../models';
 export class ForgotPasswordComponent implements OnInit {
   forgotPasswordForm!: FormGroup;
   isSuccess: boolean = false;
+  validator!:GenericValidator;
+  validationErrors : {[key: string]: string} = {};
   private errorMessageSubject = new Subject<string>();
   errorMessage$ = this.errorMessageSubject.asObservable();
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {}
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    // this.validator = new GenericValidator()
+  }
 
   ngOnInit(): void {
     this.forgotPasswordForm = this.fb.group({
       email: ['', [Validators.email, Validators.required]],
     });
-
   }
 
   onSubmit() {
     let forgotPasswordDto: ForgotPasswordDto = {
-      Email: this.forgotPasswordForm.get('email')?.value,
-      ResetPasswordClientURI: 'https://localhost:4200/users/resetPassword',
+      email: this.forgotPasswordForm.get('email')?.value,
+      resetPasswordClientURI: 'https://localhost:4200/users/reset-password',
     };
 
     this.authService.forgotPasswordRequest(forgotPasswordDto).subscribe({
-      error: (err: HttpErrorResponse) =>this.errorMessageSubject.next("Invalid Request")
+      error: (err: HttpErrorResponse) => {
+        if (err.status === 400)
+          this.errorMessageSubject.next(
+            'Email Address is not associated with an EventHub Account'
+          );
+      },
     });
   }
 }
