0bd2de9071d9c3238b757b70a84ccfcb4fff3d8c
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-01-24 01:21:37 +0200

    configure ngrx devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,13 @@ import { StoreModule } from '@ngrx/store';
 import { UserModule } from './user/user.module';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './shared/material/material.module';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -21,9 +22,17 @@ import { MaterialModule } from './material/material.module';
     FlexLayoutModule,
     MaterialModule,
     UserModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+  StoreDevtoolsModule.instrument({
+    name:'EventHub App DevTools',
+    maxAge:25,
+    logOnly:environment.production
+  })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+
+],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
