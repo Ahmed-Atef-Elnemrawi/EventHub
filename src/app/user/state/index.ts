8de484c6a03a29cb34f633284c3b24c1f918fcc7
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-01-24 14:34:16 +0200

    update selectors

--- src/app/user/state/index.ts
+++ src/app/user/state/index.ts
@@ -1,15 +1,48 @@
-import { createFeature, createFeatureSelector, createSelector } from "@ngrx/store";
-import { UserState } from "./user.reducer";
+import { state } from '@angular/animations';
+import {
+  createFeature,
+  createFeatureSelector,
+  createSelector,
+} from '@ngrx/store';
+import { UserState } from './user.reducer';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 
 const getUserFeatureState = createFeatureSelector<UserState>('user');
 
 export const getIsAuthenticated = createSelector(
   getUserFeatureState,
-  state => state.isAuthenticated
-)
+  (state) => state.isAuthenticated
+);
 
 export const getError = createSelector(
   getUserFeatureState,
-  state => state.error
-)
+  (state) => state.error
+);
+
+export const getUserProfile = createSelector(
+  getUserFeatureState,
+  (state) => state.userProfile
+);
+
+export const getUserId = createSelector(
+  getUserFeatureState,
+  (state) => state.userProfile.id
+);
+
+export const getAccessToken = createSelector(
+  getUserFeatureState,
+  (state) => state.tokenDto.accessToken
+);
+
+export const getToken = createSelector(
+  getUserFeatureState,
+  (state) => state.tokenDto
+);
+
+export const getMessage = createSelector(
+  getUserFeatureState,
+  (state) => state.message
+);
+
+
