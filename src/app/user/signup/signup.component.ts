ca6e95e3f4c5ee92d69663b67a6d1a7f9053b961
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-02-04 23:28:25 +0200

    fix issue with country code not have a value when signUp submit

--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -1,64 +1,69 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, MaxLengthValidator, Validators } from '@angular/forms';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { map, Observable, Subject } from 'rxjs';
+import { map, Observable, Subject, takeUntil } from 'rxjs';
 import { GenericValidator } from 'src/app/shared/generic-validators';
 import { Country, CountryCodes } from 'src/app/shared/models/country-codes';
 import { State } from 'src/app/state/state';
-import { AuthService } from '../auth.service';
 import { signupValidationMessages, UserForRegistrationDto } from '../models';
 import { getError } from '../state';
-import * as AuthActions from '../state/actions'
+import { UserApiActions } from '../state/actions';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent implements OnInit, AfterViewInit {
+export class SignupComponent implements OnInit, AfterViewInit, OnDestroy {
   signUpForm!: FormGroup;
   hidePassword = true;
   hideCard = false;
   hideConfirm = true;
   validator!: GenericValidator;
   filteredCountries$!: Observable<Country[]> | undefined;
-  countryCode$!: Observable<Country> | undefined;
+  countryCode$!: Observable<string> | undefined;
   backendError$!: Observable<string>;
 
   private errorMessageSubject = new Subject<{ [key: string]: string }>();
   errorMessage$ = this.errorMessageSubject.asObservable();
+  private destroyed$ = new Subject<void>();
 
   constructor(private store: Store<State>, private fb: FormBuilder) {
     this.validator = new GenericValidator(signupValidationMessages);
   }
 
   ngAfterViewInit(): void {
-    this.signUpForm.valueChanges.subscribe(() => {
-      this.errorMessageSubject.next(
-        this.validator.processMessages(this.signUpForm)
-      );
-    });
+    this.signUpForm.valueChanges
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(() => {
+        this.errorMessageSubject.next(
+          this.validator.processMessages(this.signUpForm)
+        );
+      });
 
-    this.filteredCountries$ = this.signUpForm
+    this.filteredCountries$ = this.signUpForm.get('liveIn')?.valueChanges.pipe(
+      map((val: string) =>
+        CountryCodes.filter((country) =>
+          country.name.toLowerCase().startsWith(val)
+        )
+      ),
+      takeUntil(this.destroyed$)
+    );
+
+    this.signUpForm
       .get('liveIn')
       ?.valueChanges.pipe(
-        map((val: string) =>
-          CountryCodes.filter((country) =>
-            country.name.toLowerCase().startsWith(val)
-          )
-        )
-      );
-
-    this.countryCode$ = this.signUpForm
-      .get<string>('liveIn')
-      ?.valueChanges.pipe(
-        map(
-          (val: string) =>
-            CountryCodes.filter((c) =>
-              c.name.toLowerCase().trim().includes(val.toLowerCase().trim())
-            )[0]
-        )
+        map((val) => CountryCodes.filter((c) => c.name === val).shift()),
+        takeUntil(this.destroyed$)
+      )
+      .subscribe((val) =>
+        this.signUpForm.patchValue({
+          phoneGroup: { countryCode: val?.dial_code },
+        })
       );
   }
 
@@ -110,10 +115,10 @@ export class SignupComponent implements OnInit, AfterViewInit {
           '',
           [Validators.required, Validators.pattern(/^[0-9]{10}$/)],
         ],
-        countryCode: ['+20'],
+        countryCode: '+20',
       }),
       age: [
-         0,
+        0,
         [
           Validators.required,
           Validators.min(11),
@@ -125,28 +130,29 @@ export class SignupComponent implements OnInit, AfterViewInit {
       liveIn: ['Egypt', Validators.required],
       roles: [' ', Validators.required],
     });
+  }
 
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
   onSubmit() {
     if (this.signUpForm && this.signUpForm.valid) {
-
       const phoneGroup = this.signUpForm.get('phoneGroup');
       const code = phoneGroup?.get('countryCode')?.value;
-      const phoneNumber = phoneGroup?.get('phoneNumber')?.value ;
+      const phoneNumber = phoneGroup?.get('phoneNumber')?.value;
       const roles = this.signUpForm.get('roles')?.value;
 
       let userForRegistrationDto: UserForRegistrationDto = {
         ...this.signUpForm.value,
         genre: +this.signUpForm.get('genre')?.value,
-        roles: (roles !== ' ')? [roles]:[],
+        roles: roles !== ' ' ? [roles] : [],
         phoneNumber: code + phoneNumber,
       };
 
-     this.store.dispatch(AuthActions.signup({user: userForRegistrationDto}))
-     this.backendError$ = this.store.select(getError);
+      this.store.dispatch(UserApiActions.signup({ user: userForRegistrationDto }));
+      this.backendError$ = this.store.select(getError);
     }
   }
-
-
 }
