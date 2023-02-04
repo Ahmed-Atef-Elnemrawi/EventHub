4116ca2f12fffc00466b6986355ab40c2442ce24
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-02-04 22:14:55 +0200

    update auth service - add function to update user - add function to check if the user authenticated - add function to get the userId -add function to refresh user token

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,13 +1,13 @@
-import {
-  HttpClient,
-} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import {
-  AuthResponse,
   ForgotPasswordDto,
   ResetPasswordDto,
+  TokenDto,
   UserForAuthDto,
   UserForRegistrationDto,
+  UserProfile,
 } from './models';
 
 @Injectable({
@@ -16,10 +16,18 @@ import {
 export class AuthService {
   private rootPath = 'https://localhost:5001/api/users';
 
-  constructor(private http: HttpClient) {}
+  constructor(private jwtHelper: JwtHelperService, private http: HttpClient) {}
 
   login = (userForAuth: UserForAuthDto) => {
-    return this.http.post<AuthResponse>(`${this.rootPath}/login`, userForAuth);
+    return this.http.post<TokenDto>(`${this.rootPath}/login`, userForAuth);
+  };
+
+  loadUser = (id: string) => {
+    return this.http.get<UserProfile>(`${this.rootPath}/${id}`, {
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
   };
 
   forgotPasswordRequest = (forgotPasswordDto: ForgotPasswordDto) => {
@@ -38,5 +46,42 @@ export class AuthService {
 
   registerUser = (userForRegistration: UserForRegistrationDto) => {
     return this.http.post<void>(`${this.rootPath}/signup`, userForRegistration);
+  };
+
+  updateUser = (userProfile: UserProfile, userId: string) => {
+    return this.http.put(`${this.rootPath}/${userId}`, userProfile);
+  };
+
+  isAuthenticated = (): boolean => {
+    let token = localStorage.getItem('token');
+    if (token !== null) {
+      let accessToken = (JSON.parse(token) as TokenDto).accessToken;
+      if (!this.jwtHelper.isTokenExpired(accessToken)) {
+        localStorage.setItem('isAuthenticated', 'true');
+        return true;
+      }
+      localStorage.setItem('isAuthenticated', 'false');
+      return false;
+    }
+    localStorage.setItem('isAuthenticated', 'false');
+    return false;
+  };
+
+  getUserId = (): string => {
+    let token = localStorage.getItem('token');
+    console.log(this.jwtHelper.decodeToken(token!))
+    if (token !== null) {
+      let accessToken = (JSON.parse(token) as TokenDto).accessToken;
+      return this.jwtHelper.decodeToken(accessToken)[
+        'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'
+      ];
+    }
+    return '';
+  };
+
+  refreshToken = (token: TokenDto) => {
+    if (!this.jwtHelper.isTokenExpired(token.accessToken)) return;
+
+    return this.http.post('api/token/refresh', token);
   };
 }
