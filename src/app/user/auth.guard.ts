d0b833323b187b3a297986ccea7183715dfe79da
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-02-04 22:08:44 +0200

    update auth guard - check if the user is authenticated - return to login page if the user hasn't

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,15 +1,43 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { State } from '../state/state';
+import { getIsAuthenticated } from './state';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, OnDestroy {
+  private destroyed$ = new Subject();
+  private result!: boolean;
+  constructor(private store: Store<State>, private router: Router) {}
+  ngOnDestroy(): void {
+    this.destroyed$.next(null);
+    this.destroyed$.complete();
+
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot,
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+
+    this.store
+      .select(getIsAuthenticated)
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(res => this.result = res);
+
+    this.router.navigateByUrl('users/login')
+    return this.result;
   }
-  
 }
