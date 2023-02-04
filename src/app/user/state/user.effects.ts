24ad106217a231570883893b29ad3a77465f9299
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-02-04 23:23:01 +0200

    add load user effect

--- src/app/user/state/user.effects.ts
+++ src/app/user/state/user.effects.ts
@@ -1,59 +1,85 @@
 import { Injectable } from '@angular/core';
 import { Actions } from '@ngrx/effects';
 import { createEffect, ofType } from '@ngrx/effects';
-import * as AuthActions from '../state/actions';
-import { JwtHelperService } from '@auth0/angular-jwt';
+import { UserApiActions } from '../state/actions';
 import { AuthService } from '../auth.service';
-import { catchError, exhaustMap, map, of, tap } from 'rxjs';
+import { catchError, concatMap, debounceTime, exhaustMap, map, of, tap } from 'rxjs';
 import { Router } from '@angular/router';
-import { createAction } from '@ngrx/store';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class UserEffects {
   constructor(
     private authService: AuthService,
     private actions$: Actions,
-    private jwtHelper: JwtHelperService,
     private router: Router
   ) {}
 
   loginUser$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(AuthActions.login),
+      ofType(UserApiActions.login),
       exhaustMap((action) =>
         this.authService.login(action.user).pipe(
-          map((authResponse) => {
-            localStorage.setItem('access_token', authResponse.tokenDto.accessToken);
-            this.router.navigateByUrl(
-              `/users/${authResponse.userProfile.userName}`
-            );
-            return AuthActions.loginSuccess({ authResponse });
+          map((token) => {
+            localStorage.clear();
+            localStorage.setItem('token', JSON.stringify(token));
+
+            //TODO: redirect to home page
+            this.router.navigateByUrl(``);
+            return UserApiActions.loginSuccess({ token });
           }),
-          catchError((error) => of(AuthActions.loginFailure({ error })))
+          catchError((error) => of(UserApiActions.loginFailure({ error })))
         )
       )
     );
   });
 
+  loadUser$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(UserApiActions.loadUser),
+      exhaustMap(() => {
+        if (this.authService.isAuthenticated()) {
+          let userId = this.authService.getUserId();
+
+          return this.authService.loadUser(userId).pipe(
+            map((profile) => {
+              this.router.navigateByUrl(
+                `users/${profile.userName}/event-i-attends`
+              );
+              return UserApiActions.loadUserSuccess({ profile, userId });
+            }),
+            catchError((error) => of(UserApiActions.loadUserFailure({ error })))
+          );
+        }
+        return of(
+          UserApiActions.loadUserFailure({ error: "can't load the user!" })
+        );
+      })
+    )
+  );
+
   logoutUser$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(AuthActions.logout),
+      ofType(UserApiActions.logout),
       map(() => {
         localStorage.removeItem('token');
-        return AuthActions.logoutSuccess();
+        return UserApiActions.logoutSuccess();
       }),
       tap(() => this.router.navigateByUrl('/users/login')),
-      catchError((error) => of(AuthActions.logoutFailure()))
+      catchError(() => of(UserApiActions.logoutFailure()))
     );
   });
 
   registerUser$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(AuthActions.signup),
+      ofType(UserApiActions.signup),
       exhaustMap((action) =>
         this.authService.registerUser(action.user).pipe(
-          map(() => AuthActions.signupSuccess()),
-          catchError((error) => of(AuthActions.signupFailure({ error })))
+          map(() => {
+            this.router.navigateByUrl("users/login")
+            return UserApiActions.signupSuccess();
+          }),
+          catchError((error) => of(UserApiActions.signupFailure({ error })))
         )
       )
     );
@@ -61,12 +87,12 @@ export class UserEffects {
 
   forgotPassword$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(AuthActions.forgotPassword),
+      ofType(UserApiActions.forgotPassword),
       exhaustMap((action) =>
         this.authService.forgotPasswordRequest(action.forgotPasswordDto).pipe(
-          map(() => AuthActions.forgotPasswordSuccess()),
+          map(() => UserApiActions.forgotPasswordSuccess()),
           catchError((error) =>
-            of(AuthActions.forgotPasswordFailure({ error }))
+            of(UserApiActions.forgotPasswordFailure({ error }))
           )
         )
       )
@@ -75,15 +101,33 @@ export class UserEffects {
 
   resetPassword$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(AuthActions.resetPassword),
+      ofType(UserApiActions.resetPassword),
       exhaustMap((action) =>
         this.authService.resetPassword(action.resetPasswordDto).pipe(
           map(() => {
             this.router.navigateByUrl('/users/login');
-            return AuthActions.forgotPasswordSuccess();
+            return UserApiActions.forgotPasswordSuccess();
           }),
           catchError((error) =>
-            of(AuthActions.forgotPasswordFailure({ error }))
+            of(UserApiActions.forgotPasswordFailure({ error }))
+          )
+        )
+      )
+    );
+  });
+
+  updateUser$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(UserApiActions.updateUserProfile),
+      concatMap((action) =>
+        this.authService.updateUser(action.userProfile, action.userId).pipe(
+          map(() =>
+            UserApiActions.UpdateUserProfileSuccess({
+              userProfile: action.userProfile,
+            })
+          ),
+          catchError((err: HttpErrorResponse) =>
+            of(UserApiActions.updateUserProfileFailure({ error: err.message }))
           )
         )
       )
