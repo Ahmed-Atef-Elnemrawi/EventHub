bb308194b75cd6f7f11d8492403f7c8f93a5e626
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-10 00:00:17 +0200

    feat(app): dispatch UserApiActions.loadUser in ngOnInit()
    feat(app): dispatch UserApiActions.loadUser in ngOnInit() if user is authenticated
    
    This commit dispatches UserApiActions.loadUser action to the store in the ngOnInit() method of the AppComponent only if the user is authenticated. This loads the user data when the application starts for authenticated users.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { State } from './state/app.state';
+import { UserAPIActions } from './user/state/actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'EventHub';
 
-
+  constructor(private store: Store<State>, private router: Router) {}
+  ngAfterViewInit(): void {}
+  ngOnInit(): void {
+    const isAuth = localStorage.getItem('isAuthenticated');
+    if (isAuth) this.store.dispatch(UserAPIActions.loadUser());
+  }
 }
