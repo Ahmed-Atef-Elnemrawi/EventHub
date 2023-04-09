b155d7a56ec37922a832a8ce675a23fe8a269ded
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-10 00:17:40 +0200

    feat(app): add AuthInterceptor into AppModule providers
    This commit adds the AuthInterceptor into the AppModule providers array. This interceptor adds an Authorization header to all HTTP requests to authenticate the user.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,15 @@ import { MaterialModule } from './shared/material/material.module';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
+import { NavbarModule } from './navbar/navbar.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { ArtistHomeModule } from './artist-home/artist-home.module';
+import { AuthInterceptor } from './shared/auth-interceptor';
+import { JwtModule } from '@auth0/angular-jwt';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,17 +28,31 @@ import { environment } from 'src/environments/environment';
     FlexLayoutModule,
     MaterialModule,
     UserModule,
+    NavbarModule,
+    ArtistHomeModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
-  StoreDevtoolsModule.instrument({
-    name:'EventHub App DevTools',
-    maxAge:25,
-    logOnly:environment.production
-  })
+    StoreDevtoolsModule.instrument({
+      name: 'EventHub App DevTools',
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+
+      JwtModule.forRoot({
+      config: {
+        tokenGetter: () => null,
+        allowedDomains: ['https://localhost:5001'],
+        disallowedRoutes: [],
+      },
+    }),
   ],
   providers: [
-
-],
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
