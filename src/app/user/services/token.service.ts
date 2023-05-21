37d86e5d0752d87ce1827cbef3e9dfdac4f7bc5d
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-05-21 19:31:16 +0300

    feat(TokenService): Use environment variable for BackEnd API Path
    This commit updates the TokenService in our Angular app to use an environment variable for the BackEnd API Path. This will allow us to easily switch between different API endpoints for different environments (e.g. development, production) without having to modify the code.

--- src/app/user/services/token.service.ts
+++ src/app/user/services/token.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TokenDto } from '../models';
+import { environment } from 'src/environments/environment';
 
+const ROOT_PATH = environment.apiUrl;
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
-  rootPath = 'https://localhost:5001/api/token'
+  rootPath = `${ROOT_PATH}/token`;
   constructor(private http:HttpClient) { }
 
   refreshToken = (token:TokenDto)=>{
