a037d3bcd70425757b26e4754e644072ba16bb71
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-05-21 19:50:52 +0300

    feat(SharedFeature): Declare and export camelCasePipe and DaysLeft pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CamelCasePipe } from './camel-case.pipe';
 import { DaysLeftPipe } from './days-left.pipe';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
-  declarations: [CamelCasePipe, DaysLeftPipe],
+  declarations: [CamelCasePipe, DaysLeftPipe, LoadingComponent],
   imports: [CommonModule],
-  exports: [CamelCasePipe, DaysLeftPipe],
+  exports: [CamelCasePipe, DaysLeftPipe, LoadingComponent],
 })
 export class SharedModule {}
